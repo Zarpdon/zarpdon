43e0fb4296b627dcad734a2f3afb483f548e7d0e
Zarpdon <zarpdon.office@gmail.com>
2026-06-21 21:58:08 -0300

    chore: add eslint, but eslint d'ont fix on save; add prettier; shadcn setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const interHeading = Inter({ subsets: ["latin"], variable: "--font-heading" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn(interHeading.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

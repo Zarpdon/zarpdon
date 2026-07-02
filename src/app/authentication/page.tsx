0a09635ea9e3c2e70134dcdf08b4f9355f3d4c08
Zarpdon <zarpdon.office@gmail.com>
2026-07-02 19:52:20 -0300

    feat: add header; header's menu; showing account avatar and sign-in/sign-up in menu

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+
 import { Header } from "@/components/common/header";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignInForm from "./components/sign-in-form";
@@ -8,7 +11,6 @@ const Authentication = async () => {
   return (
     <>
       <Header />
-
       <div className="flex w-full flex-col gap-6 p-5">
         <Tabs defaultValue="sign-in">
           <TabsList>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+
+import { Header } from "@/components/common/header";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple Next.js app.</p>
-      <Button>Confirmar</Button>
+      <div>
+        <Header />
+      </div>
+      <h1>Página inicial</h1>
+      <p>
+        This is a simple Next.js app. <br />
+        Esta é uma aplicação simples feita com Next.js.
+      </p>
+      <Button>
+        <Link href="/authentication">Autenticação</Link>
+      </Button>
     </div>
   );
 };
